b9e59687fb3bcff623e65da9fbcc3827857c6b4c
Ducanh1994 <121839766+Ducanh1994@users.noreply.github.com>
2023-04-20 12:48:50 +0700

    New

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Employee = require('/models/employee.model')
 router.get('/',(req,res) => {
-    res.json('sample text');
+    res.render('employee/addOrEdit',{
+        viewTitle :'Insert Employee'
+    })
 })
+router.post('/',(req,res) => {
+    let employee = new Employee();
+    employee.fullname = req.body.fullname;
+    employee.email = req.body.email;
+    employee.mobile = req.body.mobile;
+    employee.city = req.body.city;
+})
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,19 @@
-require ('./models/db');
+require('./models/db');
 const express = require('express');
 const app = express();
-const employeeController = require('./controllers/employeeController')
-app.listen(3000,()=>{
+const employeeController = require('./controllers/employeeController');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const parser = require('body-parser');
+const bodyParser = require("body-parser");
+app.listen(3000, () => {
     console.log('Server is running at port 3000')
-})
-app.use('/employee',employeeController)
+});
+app.use('/employee', employeeController);
+app.set('views', path.join(__dirname, '/views'));
+app.engine('hbs', exphbs({extname: 'hbs', defaultLayout: 'mainLayout',
+    layoutsDir: __dirname + '/views/layouts/'}));
+// chu thich lai
+app.set('view engine','hbs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json);
