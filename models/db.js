46d73d2f324f48dff609f7e8424dce06a9f6fde7
Ducanh1994 <121839766+Ducanh1994@users.noreply.github.com>
2023-04-20 15:11:06 +0700

    New

--- models/db.js
+++ models/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-require ('./employee.model');
-mongoose.connect('mongodb://127.0.0.1:27017/EmployeeDB',{useNewUrlParser:true}, (err)=>{
-    if (!err){
-        console.log('MongooseDB Connection success')}
-    else {
-        console.log('Error In DB Connection '+ err)}
+
+mongoose.connect('mongodb://127.0.0.1:27017/EmployeeDB', { useNewUrlParser: true }, (err) => {
+    if (!err) { console.log('MongoDB Connection Succeeded.') }
+    else { console.log('Error in DB connection : ' + err) }
 });
+
+require('./employee.model');
--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 require('./models/db');
+
 const express = require('express');
-const app = express();
-const employeeController = require('./controllers/employeeController');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const parser = require('body-parser');
-const bodyParser = require("body-parser");
+const bodyparser = require('body-parser');
+
+const employeeController = require('./controllers/employeeController');
+
+var app = express();
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+app.use(bodyparser.json());
+app.set('views', path.join(__dirname, '/views/'));
+app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
+app.set('view engine', 'hbs');
+
 app.listen(3000, () => {
-    console.log('Server is running at port 3000')
+    console.log('Express server started at port : 3000');
 });
+
 app.use('/employee', employeeController);
-app.set('views', path.join(__dirname, '/views'));
-app.engine('hbs', exphbs({extname: 'hbs', defaultLayout: 'mainLayout',
-    layoutsDir: __dirname + '/views/layouts/'}));
-// chu thich lai
-app.set('view engine','hbs');
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json);
